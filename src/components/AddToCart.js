572fdba4f74277e44704f778df43e1248bc26309
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-06-22 08:41:02 +0400

    adding cart functionality

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa";
@@ -6,6 +6,7 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
@@ -57,7 +58,11 @@ const AddToCart = ({ product }) => {
           decrease={decrease}
         />
 
-        <Link to="/cart" className="btn">
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
